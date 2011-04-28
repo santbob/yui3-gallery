a297649d4971f3914fb806a9444b116d77c49ad1
Santhosh <santhoshbabun@gmail.com>
2011-04-28 01:36:31 -0700

    first checkin for the lazyloadimages module

---
+++ src/gallery-lazyloadimages/js/lazyloadimages.js
@@ -0,0 +1,34 @@
+var LazyLoadImages = {
+   /**
+    * Fetches the Images for the img tags with attribute data-src immediately,
+    * should be used when calling this module on after loading the page.
+    *
+    * @param {String} selecter is the context selecter for the img tag to be lazy loaded
+    *
+    */
+   processnow: function(selecter) {
+	var imgselecter = "img[xrc]";
+	imgselecter=(selecter)?selecter+" "+imgselecter:imgselecter; 
+	Y.all(imgselecter).each(function (el, i) {
+		var url = el.getAttribute('xrc');
+		if(url){
+			el.setAttribute('src', url);
+			el.removeAttribute('xrc');
+		}
+	});	
+   },
+   
+   /**
+    * Fetches the Images for the img tags with attribute data-src immediately,
+    * should be used when calling this module on after loading the page.
+    *
+    * @param {String} selecter is the context selecter for the img tag to be lazy loaded 
+    *
+    */
+   processAfterLoad: function(selecter){
+	Y.on("domready", LazyLoadImages.processnow(selecter));
+   }
+};
+
+// Alias it on YUI instance.
+Y.lazyloadimages = LazyLoadImages;
